7d3f09a55d51dac212a29cd4c72e60f493638952
HITGIF <carbonylgroup@outlook.com>
2017-09-07 18:08:18 +0800

    fix #8 Reuse object in onDraw()

--- textfieldboxes/src/main/java/studio/carbonylgroup/textfieldboxes/ClipToBoundsView.java
+++ textfieldboxes/src/main/java/studio/carbonylgroup/textfieldboxes/ClipToBoundsView.java
@@ -3,6 +3,7 @@ package studio.carbonylgroup.textfieldboxes;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Path;
+import android.graphics.Rect;
 import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.widget.RelativeLayout;
@@ -14,31 +15,49 @@ import android.widget.RelativeLayout;
 public class ClipToBoundsView extends RelativeLayout {
 
     Context context;
+    Path clipPath = new Path();
+    Rect rect = new Rect();
+    RectF rectF = new RectF();
+    Float cornerRadius;
+    Canvas canvas;
 
     public ClipToBoundsView(Context context) {
 
         super(context);
         this.context = context;
+        init();
     }
 
     public ClipToBoundsView(Context context, AttributeSet attrs) {
 
         super(context, attrs);
         this.context = context;
+        init();
     }
 
     public ClipToBoundsView(Context context, AttributeSet attrs, int defStyleAttr) {
 
         super(context, attrs, defStyleAttr);
         this.context = context;
+        init();
+    }
+
+    protected void init() {
+        cornerRadius = context.getResources().getDimension(R.dimen.text_field_boxes_corner_radius);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+
+        super.onSizeChanged(w, h, oldw, oldh);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
 
-        Path clipPath = new Path();
-        Float cornerRadius = context.getResources().getDimension(R.dimen.text_field_boxes_corner_radius);
-        clipPath.addRoundRect(new RectF(canvas.getClipBounds()), cornerRadius, cornerRadius, Path.Direction.CW);
+        canvas.getClipBounds(rect);
+        rectF.set(rect);
+        clipPath.addRoundRect(rectF, cornerRadius, cornerRadius, Path.Direction.CW);
         canvas.clipPath(clipPath);
         super.onDraw(canvas);
     }
